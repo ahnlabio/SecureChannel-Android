f3dc369847dbad9bb860dabfcd261ad38f34ecd7
JinGyuriShin <jin@ahnlab.io>
2023-11-29 14:58:02 +0900

    fix the build script

--- securechannel/build.gradle.kts
+++ securechannel/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 
 val mGroupId = "io.myabcwallet"
 val mArtifactId = "securechannel"
-val mVersionCode = 5
-val mVersionName = "0.1.4"
+val mVersionCode = 6
+val mVersionName = "0.1.5"
 val libraryName = "SecureChannel-Android"
 val libraryDescription = "Library for Android to create a Secure Channel to communicate with WAAS"
 
