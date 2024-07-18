c3f0325aa871c500a3b276a868b6bf1d0338c00b
JinGyuriShin <jin@ahnlab.io>
2024-07-18 18:19:14 +0900

    버전업

--- securechannel/build.gradle.kts
+++ securechannel/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 
 val mGroupId = "io.myabcwallet"
 val mArtifactId = "securechannel"
-val mVersionCode = 9
-val mVersionName = "0.1.9"
+val mVersionCode = 10
+val mVersionName = "0.1.10"
 val libraryName = "SecureChannel-Android"
 val libraryDescription = "Library for Android to create a Secure Channel to communicate with WAAS"
 
